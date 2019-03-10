d80434bbd678595f44559035ce88e0e477e3eacc
Sarina <sarina.motahedin@gmail.com>
2019-03-09 21:32:30 -0500

    JDBC DAY1

---
+++ src/test/java/JDBCDAY1.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+
+import org.junit.Test;
+
+public class JDBCDAY1 {
+
+    String url = "jdbc:postgresql://room-reservation-qa.cxvqfpt4mc2y.us-east-1.rds.amazonaws.com:5432/hr";
+    String username = "hr";
+    String password = "hr";
+
+    @Test
+    public void jdbcTest1() throws SQLException{
+        //Now we are connecting to the database with java code and JDBC API
+        Connection connection = DriverManager.getConnection(url,username,password);
+        //We need to create a statement
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        //now we are going to run a query, so for that we need to create a result set
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM employees;");
+        //we need to skip first record, because it starts from 0
+        resultSet.next();
+        //we are getting first record based on the column name
+        String value = resultSet.getString("first_name");
+        //just to output result
+        System.out.println(value);
+    }
+
+
+
+}
---
+++ src/test/java/MurodilJDBC.java
@@ -0,0 +1,105 @@
+import org.junit.Test;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MurodilJDBC {
+    String url = "jdbc:postgresql://room-reservation-qa.cxvqfpt4mc2y.us-east-1.rds.amazonaws.com:5432/hr";
+    String username = "hr";
+    String password = "hr";
+
+    @Test
+    public void postgresqlJDBC() throws SQLException {
+        Connection connection = DriverManager.getConnection(url, username, password);
+       // Statement statement = connection.createStatement();
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+        ResultSet resultSet = statement.executeQuery("Select * from countries;");
+
+
+        resultSet.last();
+        int rowsCount = resultSet.getRow();
+        System.out.println("Number of rows: " + rowsCount);
+
+
+        resultSet.beforeFirst();
+        while(resultSet.next()) {
+            System.out.println(resultSet.getString(1)+
+                                "-"+ resultSet.getString("country_name") +
+                                "-"+ resultSet.getInt("region_id"));
+        }
+
+        
+
+//        resultSet.next();
+//        System.out.println(resultSet.getRow()); // --> 1
+//
+//        resultSet.previous();//all next 3 actions will not work FORWARD_ONLY resultset.
+//        resultSet.first();
+//        resultSet.last();
+//        System.out.println(resultSet.getRow());
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+
+
+    @Test
+    public void JDBCTEST2() throws SQLException{
+        Connection connection = DriverManager.getConnection(url, username, password);
+        // Statement statement = connection.createStatement();
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+        String sql = "Select employee_id, last_name, job_id, salary from employees;";
+       // String sql = "Select * from employees;";
+
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        //Database metadata
+        DatabaseMetaData dbMetadata = connection.getMetaData();
+        System.out.println("USER: " + dbMetadata.getUserName());
+        System.out.println("DATABASE TYPE: " + dbMetadata.getDatabaseProductName());
+
+
+        //resultSet metadata
+        ResultSetMetaData rsMetadata = resultSet.getMetaData();
+        System.out.println("COLUMNS COUNT: " + rsMetadata.getColumnCount());
+        System.out.println(rsMetadata.getColumnName(1));
+
+        //print all column names using a loop
+        for(int i=1; i<= rsMetadata.getColumnCount(); i++){
+            System.out.println(i + " -> " + rsMetadata.getColumnName(i));
+        }
+
+
+        //throw resultset into a list of maps
+        //create a list of maps
+        List<Map<String,Object>> list = new ArrayList<>();
+        ResultSetMetaData rsMdata = resultSet.getMetaData();
+
+        int colCount = rsMdata.getColumnCount();
+
+        while(resultSet.next()){
+            Map<String,Object> rowMap = new HashMap<>();
+            for(int col = 1; col <= colCount; col++){
+                rowMap.put(rsMdata.getColumnName(col), resultSet.getObject(col));
+            }
+
+            list.add(rowMap);
+
+        }
+
+
+//        resultSet.absolute(rowNum);
+
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+    }
+}
